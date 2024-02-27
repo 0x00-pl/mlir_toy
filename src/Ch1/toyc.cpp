b4de5a2ac49f431b47429bbd496ae6e572c39525
pl <0x00.pl@gmail.com>
2024-02-28 01:10:26 +0800

    reformat

--- src/Ch1/toyc.cpp
+++ src/Ch1/toyc.cpp
@@ -31,17 +31,19 @@ static cl::opt<std::string> inputFilename(cl::Positional,
                                           cl::init("-"),
                                           cl::value_desc("filename"));
 namespace {
-enum Action { None, DumpAST };
+    enum Action {
+        None, DumpAST
+    };
 } // namespace
 
 static cl::opt<enum Action>
-    emitAction("emit", cl::desc("Select the kind of output desired"),
-               cl::values(clEnumValN(DumpAST, "ast", "output the AST dump")));
+        emitAction("emit", cl::desc("Select the kind of output desired"),
+                   cl::values(clEnumValN(DumpAST, "ast", "output the AST dump")));
 
 /// Returns a Toy AST resulting from parsing the file or a nullptr on error.
 std::unique_ptr<toy::ModuleAST> parseInputFile(llvm::StringRef filename) {
   llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> fileOrErr =
-      llvm::MemoryBuffer::getFileOrSTDIN(filename);
+          llvm::MemoryBuffer::getFileOrSTDIN(filename);
   if (std::error_code ec = fileOrErr.getError()) {
     llvm::errs() << "Could not open input file: " << ec.message() << "\n";
     return nullptr;
@@ -60,11 +62,11 @@ int main(int argc, char **argv) {
     return 1;
 
   switch (emitAction) {
-  case Action::DumpAST:
-    dump(*moduleAST);
-    return 0;
-  default:
-    llvm::errs() << "No action specified (parsing only?), use -emit=<action>\n";
+    case Action::DumpAST:
+      dump(*moduleAST);
+      return 0;
+    default:
+      llvm::errs() << "No action specified (parsing only?), use -emit=<action>\n";
   }
 
   return 0;
